29770dc9589a77802ac54ca775654547a120459f
mansigarg10 <145923366+mansigarg10@users.noreply.github.com>
2024-07-13 18:50:44 +0530

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -3,9 +3,14 @@
 
 /*
 * Write your query here
-*/
+*/ SELECT firstname,income,age FROM customers
+WHERE income< 50000
+AND age BETWEEN 30 AND 50;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
-/*
+/* 59361.925908612832
 * Write your query here
-*/
+*/SELECT AVG(income) FROM customers
+WHERE age BETWEEN 20 AND 50;
+
+
